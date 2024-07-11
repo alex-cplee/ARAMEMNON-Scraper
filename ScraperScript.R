2f7c8bce0a244f3cacb26eaccd0aaaaf3aed22a1
Alex Lee <93962658+alex-cplee@users.noreply.github.com>
2024-07-11 22:34:37 +1000

    update ARAMEMNON website

--- ScraperScript.R
+++ ScraperScript.R
@@ -4,7 +4,7 @@ ProteinType = c()
 NumberOfDomains = c()
 
 for (i in gene) {
-  AramemHTML = read_html(paste0('http://aramemnon.uni-koeln.de/seq_view.ep?search=',i))
+  AramemHTML = read_html(paste0('https://aramemnon.botanik.uni-koeln.de/seq_view.ep?search=',i))
   Result <- AramemHTML %>% html_nodes('p')
   ResultText <- gsub('[\t\n]', '', html_text(Result[grep("headtopbox", Result)]))
   if (grepl("protein result(s)", ResultText,  fixed = TRUE) == 1) {
@@ -22,7 +22,7 @@ for (i in gene) {
       DomainSearch <- AramemHTML %>%  html_nodes(".sideTS") %>% html_element("a") %>% html_attr("href")
       DomainSearch <- str_split(DomainSearch[1], "\" ")
       DomainSearch <- unlist(str_extract_all(DomainSearch[[1]][1],"\\(?[0-9]+\\)?"))
-      DomainSearchNextPage <- read_html(paste0('http://aramemnon.uni-koeln.de/tm_sub.ep?GeneID=', DomainSearch[1], '&ModelID=0'))
+      DomainSearchNextPage <- read_html(paste0('https://aramemnon.botanik.uni-koeln.de/tm_sub.ep?GeneID=', DomainSearch[1], '&ModelID=0'))
       BBProtein <- DomainSearchNextPage %>% html_element('table') %>% html_nodes("div")
       BBProtein <- gsub('[\t\n]', '', html_text(BBProtein[grep("Beta barrel TM protein predictions", BBProtein)]))
       if (length(BBProtein) == 0) {
@@ -31,7 +31,7 @@ for (i in gene) {
         DomainSearchNextPage <- unlist(str_extract_all(DomainSearchNextPage[[1]][5],"\\(?[0-9]+\\)?"))
         Coordinate1 <- DomainSearchNextPage[3]
         Coordinate2 <- DomainSearchNextPage[5]
-        DomainSearchFinalPage <- read_html(paste0('http://aramemnon.uni-koeln.de/tm_sub2.ep?GeneID=', DomainSearch[1], '&ModelID=', Coordinate1, '&MethodID=', Coordinate2))
+        DomainSearchFinalPage <- read_html(paste0('https://aramemnon.botanik.uni-koeln.de/tm_sub2.ep?GeneID=', DomainSearch[1], '&ModelID=', Coordinate1, '&MethodID=', Coordinate2))
         DomainSearchFinalPage <- DomainSearchFinalPage %>% html_element('table') %>% html_nodes(".sideT")
         NumberOfDomains[i] <- max(as.numeric(grep('^-?[0-9.]+$', html_text(DomainSearchFinalPage), val = T)))
         if (NumberOfDomains[i] == '-Inf') {
