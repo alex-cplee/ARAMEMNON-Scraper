00e923cac35c5a3b60c9b6d147c6619760484c44
Alex Lee <93962658+alex-cplee@users.noreply.github.com>
2024-07-14 17:58:38 +1000

    major upgrades
    converted ScraperScript.R to function (it was a for loop before) and use lapply on search at the server side;
    fixed logics on file upload;
    added a progress bar to aid progress monitoring, useful when searching large number of accession;
    added comments to scripts

--- ARAMEMScraper_app.R
+++ ARAMEMScraper_app.R
@@ -1,7 +1,9 @@
 library(shinybusy)
 library(shiny)
+library(shinyjs)
 
 ui <- fluidPage(
+  useShinyjs(),
   add_busy_spinner(spin = "fading-circle"),
   titlePanel(title = 'ARAMEMNON Scraper App'), 
   sidebarLayout(position = "left",
@@ -14,7 +16,10 @@ ui <- fluidPage(
                   actionButton(inputId = 'example', label='Example'),
                   p(''),
                   p('or Upload a list of genes as .txt below:'),
-                  uiOutput('filedata_ui'),
+                  fileInput(inputId = "filedata",
+                            label = "Upload only a single text file containing AGI Accessions", 
+                            multiple = FALSE, 
+                            accept = c('.txt')),
                   actionButton(inputId = 'go', label='Submit'),
                   actionButton(inputId = 'resetFile', label='Clear File')),
                 mainPanel(
@@ -28,6 +33,31 @@ server <- function(input, output, session) {
   library(stringr)
   library(rvest)
   library(tidyr)
+  library(pbapply)
+  source('ScraperScript.R')
+  
+  # reset the output data frame in memory
+  output1 <- c()
+
+  # set variables to toggle on or off the file upload
+  rv <- reactiveValues(
+    data = NULL,
+    clear = TRUE
+  )
+  
+  # When there is a file in the upload section, trigger to include the file in the search
+  observeEvent(input$filedata, {
+    rv$clear <- FALSE
+  })
+  
+  # When "Clear File" is clicked, clear the text in the upload dialogue and deactivate any file directory in memory from the search 
+  observeEvent(input$resetFile, {
+    rv$data <- NULL
+    rv$clear <- TRUE
+    reset('filedata')
+  })
+  
+  # generate a list of plant accession examples 
   output$textfield_ui <- renderUI({
     input$example 
     textAreaInput(inputId = "textin",
@@ -35,43 +65,75 @@ server <- function(input, output, session) {
                   width = '100%',
                   resize = 'vertical',
                   value = 'At4g01470\nMELO3C002183\nPotri.001G235300\nSolyc06g072130\nGSVIVG01025038001\nBradi2g07830\nGSMUA_Achr9T06260\nLOC_Os01g13130\nGRMZM2G146627')})
+  
+  # clear the text upon clicking "Clear Text"
   observeEvent(input$resetText, {updateTextInput(session, 'textin', value = '')})
-  output$filedata_ui <- renderUI({
-    input$resetFile
-    fileInput(inputId = "filedata",
-              label = "Upload only a single text file containing AGI Accessions", 
-              multiple = FALSE, 
-              accept = c('.txt'))})
+  
   observeEvent(input$go, {
-    output$gene_list <- renderDataTable({
-      #only reactive when the submit button is clicked
+    withProgress(message = "Your Aramemnon query is in progess...", value=0,{
+      #the search function is only activated when the submit button is clicked at least once
       isolate(
-        if (isTruthy(input$filedata)){
+        if (rv$clear == FALSE){
+          # If there is a file being uploaded, include genes in the text-box and file in the search
           gene <<- list()
-          gene <<- unlist(read.delim(input$filedata$datapath, header = FALSE))
+          gene <<- unlist(read.delim(input$filedata$datapath, header = FALSE)) |> suppressWarnings()
           gene <<- unique(c(gene,str_split(str_replace_all(input$textin, ',', '\n'), "\n")[[1]]))
           gene <<- gene[gene != ""]
-          source('ScraperScript.R')
-          return(output1)
-          }
-        else if (input$textin == ""){
-          df = data.frame(matrix(ncol = 4, nrow = 0))
-          colnames(df) <- c('AGI_accession', 'Annotation', 'ProteinType', 'NumberOfDomains')
-          return(df)
-          }
+          #calculate the progress of the search
+          percentage <<- 0
+          wranggling <- pblapply(gene, 
+                               function(x) {
+                                 Sys.sleep(0.05);
+                                 percentage <<- percentage + 1/length(gene)*100
+                                 incProgress(1/length(gene), detail = paste0("Progress: ",round(percentage,1)))
+                                 AMNSearch(x)
+                               })
+          output1 <<- 
+            setNames(
+              as.data.frame(do.call(rbind, wranggling)), 
+              c("AGI_accession", "Annotation", "ProteinType", "NumberOfDomains")
+              )
+          } 
+        else if (input$textin == "" || is.na(input$textin)){
+          # create an empty table if no genes are passed into the search
+          percentage <<- 100
+          output1 <<- data.frame(matrix(ncol = 4, nrow = 0))
+          colnames(output1) <<- c('AGI_accession', 'Annotation', 'ProteinType', 'NumberOfDomains')
+          } 
         else {
+          # If there is no file being uploaded, include genes only in the text-box in the search
           gene <<- list()
           gene <<- str_split(str_replace_all(input$textin, ',', '\n'), "\n")[[1]]
-          source('ScraperScript.R')
-          return(output1)}
+          percentage <<- 0
+          wranggling <- pblapply(gene, 
+                                 function(x) {
+                                   Sys.sleep(0.05);
+                                   percentage <<- percentage + 1/length(gene)*100
+                                   incProgress(1/length(gene), detail = paste0("Progress: ",round(percentage,2)))
+                                   AMNSearch(x)
+                                 })
+          output1 <<- 
+            setNames(
+              as.data.frame(do.call(rbind, lapply(gene, AMNSearch))), 
+              c("AGI_accession", "Annotation", "ProteinType", "NumberOfDomains")
+            )
+        }
         )
-      }
-    )
+      })
+  })
+  
+  observeEvent(input$go, {
+    output$gene_list <- renderDataTable({
+      #the rsult table only appears when the submit button is clicked at least once
+      isolate(output1)
+      })
+  })
+    
+  # download function
     output$Download <- downloadHandler(
       filename = function(){"results.csv"}, 
       content = function(fname){
-        write.csv(output1, fname)})}
-  )
-}
+        write.csv(output1, fname)})
+    }
 
 shinyApp(ui = ui, server = server)
--- ScraperScript.R
+++ ScraperScript.R
@@ -1,30 +1,32 @@
-output1 <- data.frame(
-  matrix(ncol = 4,
-         nrow = 0, 
-         dimnames=list(NULL, c("AGI_accession", "Annotation", "ProteinType", "NumberOfDomains"))
-  )
-)
-
-for (i in gene) {
+AMNSearch <- function(i) {
   AramemHTML = read_html(paste0('https://aramemnon.botanik.uni-koeln.de/seq_view.ep?search=',i))
   Result <- AramemHTML %>% html_nodes('p')
   ResultText <- gsub('[\t\n]', '', html_text(Result[grep("headtopbox", Result)]))
+  
+  #Scraping initial result page provided that the results are valid
   if (grepl("protein result(s)", ResultText,  fixed = TRUE) == 1) {
     AGI_Output <- AramemHTML %>% html_nodes('tr') %>% html_nodes('ul') %>% html_nodes('span')
     AGI_accession <- gsub("[\t\n]", "", html_text(AGI_Output[1]))
     Annotation_Output <- AramemHTML %>%  html_nodes(".specT")
     Annotation <-  gsub("[\t\n]", "", html_text(Annotation_Output[1]))
     DetermineProteinType <- AramemHTML %>%  html_nodes(".sideTS") %>%  html_children() %>%  html_children() %>% html_attrs()
+    
+    # In Aramemnon results page, there is a little icon in one of the columns for each protein to indicate whether it is a membrane protein or soluble protein
+    # We leverage the information from the icon to determine the membrane character of each protein. 
     if (DetermineProteinType[[2]][["src"]] == "./Gifs/T10.gif"|DetermineProteinType[[2]][["src"]] == "./Gifs/T01.gif"){
       ProteinType = 'membrane protein'
     } else{
       ProteinType = 'soluble or peripheral protein'
     }
+    
+    # If the protein is determined as membrane-bound, proceed to search for the number of transmembrane domains
     if (ProteinType == 'membrane protein'){
       DomainSearch <- AramemHTML %>%  html_nodes(".sideTS") %>% html_element("a") %>% html_attr("href")
       DomainSearch <- str_split(DomainSearch[1], "\" ")
       DomainSearch <- unlist(str_extract_all(DomainSearch[[1]][1],"\\(?[0-9]+\\)?"))
       DomainSearchNextPage <- read_html(paste0('https://aramemnon.botanik.uni-koeln.de/tm_sub.ep?GeneID=', DomainSearch[1], '&ModelID=0'))
+      
+      #need a special case for b-barrel proteins in the next two lines
       BBProtein <- DomainSearchNextPage %>% html_element('table') %>% html_nodes("div")
       BBProtein <- gsub('[\t\n]', '', html_text(BBProtein[grep("Beta barrel TM protein predictions", BBProtein)]))
       if (length(BBProtein) == 0) {
@@ -36,6 +38,10 @@ for (i in gene) {
         DomainSearchFinalPage <- read_html(paste0('https://aramemnon.botanik.uni-koeln.de/tm_sub2.ep?GeneID=', DomainSearch[1], '&ModelID=', Coordinate1, '&MethodID=', Coordinate2))
         DomainSearchFinalPage <- DomainSearchFinalPage %>% html_element('table') %>% html_nodes(".sideT")
         NumberOfDomains <- max(as.numeric(grep('^-?[0-9.]+$', html_text(DomainSearchFinalPage), val = T)))
+        
+        # For some reason ARAMEMNON classifies certain proteins as membrane proteins without a predicted transmembrane domain. 
+        # Each of these proteins have a '-INF' value because there is no membrane domain table in the last HTML link. 
+        # This step converts all '-INF' values to 0. 
         if (NumberOfDomains == '-Inf') {
           NumberOfDomains = 0
         } else {
@@ -52,9 +58,7 @@ for (i in gene) {
     NumberOfDomains = ''
   }
   Annotation <- ifelse(grepl('\\s{2}', Annotation)==TRUE, 
-                          substr(Annotation, 1, str_locate(Annotation, "\\s{2}")[1, 'start']-1), 
-                          Annotation)
-  output1 <- rbind(output1, setNames(as.list(c(AGI_accession, Annotation, ProteinType, NumberOfDomains)), names(output1)))
+                       substr(Annotation, 1, str_locate(Annotation, "\\s{2}")[1, 'start']-1), 
+                       Annotation)
+  return(c(AGI_accession, Annotation, ProteinType, NumberOfDomains))
 }
-
-return(output1)
